017fadcb83874f978058e2e70dbbd17810f89a25
Peter Kryger Kristensen <glpekri8@student.eadania.dk>
2019-04-24 10:39:05 +0200

    Here we go again

--- DungeonCrawler/MainWindow.xaml.cs
+++ DungeonCrawler/MainWindow.xaml.cs
@@ -26,16 +26,22 @@ namespace DungeonCrawler
         public int turnCount = 1;
         public int oneTurn = 1;
         public int resetTurn = 0;
-        public bool isDefending = false;
-        public int enemyDmg = 10;
+        public int skeletonDmg = 15;
+        public int spiderDmg = 17;
+        public int ghoulDmg = 30;
         public int playerDmg = 1;
-        public double defensValue = 0;
+        public int fsc = 15;
+        private bool iceSpear = false;
+        private bool earthQuake = false;
+        private bool thunderStorm = false;
         private string combatLog = "You dealt 1 dmg. Wauv you must feel good about yourself about now, huh.\n";
-        private string defenseFailLog = "You tried to defend but failed. \n";
-        private string exhaustionLog = "You are too exhausted (Too low stamina to do anything) \n)";
+        private string spellCastLog = "You dealt 3 dmg. WOOOAAAH TAHT WAS EPIC!!! \n";
+        private string OutOfManaLog = "You are out of mana, this could be troublesome... \n)";
+        private string deathLog = "Too Bad... Knew you didn't had it in ya anyways, so just give up";
         // Hub
         public static int currency = 100;
         public static int potion;
+        public static int ScoreBoard = 0;
 
         public MainWindow()
         {
@@ -43,93 +49,328 @@ namespace DungeonCrawler
             
             Currency.Content = "Gold: " + currency.ToString();
             PotionCounter.Content = "X " + potion.ToString();
-            
+            Score.Content = "Score =" + ScoreBoard.ToString();
+
+            if (Keyboard.IsKeyDown(Key.Escape))
+            {
+                ShowMenu();
+            }
         }
 
-        private void Button_Click_1(object sender, RoutedEventArgs e) //Exit button
+        //Exit button
+        private void Button_Click_1(object sender, RoutedEventArgs e) 
+        {
+            ShowMenu();
+        }
+
+        //Attack button
+        public void Attack_Click(object sender, RoutedEventArgs e) 
+        {
+            if (HealthBar.Value > 0)
+            {
+                if (SkeletonHealth.IsVisible)
+                {
+                    SkeletonHealth.Value -= playerDmg;
+                    TextBox.Text = combatLog;
+                    turnCount += oneTurn;
+                }
+
+                if (GiantSpiderHealth.IsVisible)
+                {
+                    GiantSpiderHealth.Value -= playerDmg;
+                    TextBox.Text = combatLog;
+                    turnCount += oneTurn;
+                }
+
+                if (GhoulHealth.IsVisible)
+                {
+                    GhoulHealth.Value -= playerDmg;
+                    TextBox.Text = combatLog;
+                    turnCount += oneTurn;
+                }
+            } 
+           
+        }
+
+        //Inventory(Items)
+        private void Items_Click(object sender, RoutedEventArgs e)
+        {
+            
+            if (Inventory.IsVisible)
+            {
+                Inventory.Visibility = Visibility.Hidden;
+            }
+            else
+            {
+                Inventory.Visibility = Visibility.Visible;
+            }
+
+        }
+
+        private void PotionSwift_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void PotionTough_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void PotionMana_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void EscapeRope_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        //Spells
+        private void Spells_Click(object sender, RoutedEventArgs e)
+        {
+            if (Spell_Menu1.IsVisible)
+            {
+                HideSpells();
+            }
+            else
+            {
+                ShowSpells();
+            }
+            
+
+        }
+
+        private void Icespear_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Earthquake_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ThunderStorm_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Fireball_Click(object sender, RoutedEventArgs e)
+        {
+            if (ManaBar.Value >= 15)
+            {
+                if (SkeletonHealth.IsVisible)
+                {
+                    SkeletonHealth.Value -= 3;
+                    TextBox.Text = spellCastLog;
+                    ManaBar.Value -= fsc;
+
+                }
+
+                if (GiantSpiderHealth.IsVisible)
+                {
+                    GiantSpiderHealth.Value -= 3;
+                    TextBox.Text = spellCastLog;
+                    ManaBar.Value -= fsc;
+                }
+
+                if (GhoulHealth.IsVisible)
+                {
+                    GhoulHealth.Value -= 3;
+                    TextBox.Text = spellCastLog;
+                    ManaBar.Value -= fsc;
+                }
+            }
+            else
+            {
+                TextBox.Text = OutOfManaLog;
+            }
+        }
+
+        //Enemies
+        private void SkeletonHealth_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (turnCount == 2)
+            {
+                HealthBar.Value -= skeletonDmg;
+                turnCount = resetTurn;
+            }
+
+            if (SkeletonHealth.Value <= 0)
+            {
+                SlainSkeleton();
+                ScoreBoard++;
+                currency += 2;
+                Score.Content = "Score =" + ScoreBoard.ToString();
+                Currency.Content = "Gold: " + currency.ToString();
+            }
+
+        }
+
+        private void GiantSpider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (turnCount == 2)
+            {
+                HealthBar.Value -= spiderDmg;
+                turnCount = resetTurn;
+            }
+
+            if (GiantSpiderHealth.Value <= 0)
+            {
+                SlainGiantSpider();
+                ScoreBoard++;
+                currency += 6;
+                Score.Content = "Score =" + ScoreBoard.ToString();
+                Currency.Content = "Gold: " + currency.ToString();
+            }
+
+        }
+
+        private void GhoulHealth_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (turnCount == 3)
+            {
+                HealthBar.Value -= ghoulDmg;
+                turnCount = resetTurn;
+            }
+
+            if (GhoulHealth.Value <=0)
+            {
+                ScoreBoard++;
+                currency += 12;
+                Score.Content = "Score =" + ScoreBoard.ToString();
+                Currency.Content = "Gold: " + currency.ToString();
+            }
+        }
+
+        private void SlainSkeleton()
+        {
+            GiantSpiderHealth.Visibility = Visibility.Visible;
+            GiantSpiderText.Visibility = Visibility.Visible;
+            SkeletonHealth.Visibility = Visibility.Hidden;
+            Skeleton_Text.Visibility = Visibility.Hidden;
+        }
+
+        private void SlainGiantSpider()
+        {
+            GhoulHealth.Visibility = Visibility.Visible;
+            GhoulText.Visibility = Visibility.Visible;
+            GiantSpiderHealth.Visibility = Visibility.Hidden;
+            GiantSpiderText.Visibility = Visibility.Hidden;
+        }
+
+        //Player
+        private void HealthBar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            health = HealthBar.Value;
+            if (HealthBar.Value == 0)
+            {
+                TextBox.Text = deathLog;
+            }
+        }
+
+        //Return to Hub
+        private void Return_Button(object sender, RoutedEventArgs e)
         {
             Close_Dungeon();
         }
 
-        public void Attack_Click(object sender, RoutedEventArgs e) //Attack button
-        {
-           EnemyHealth1.Value -= playerDmg;
-           TextBox.Text = combatLog;
-           turnCount += oneTurn;
-        }
 
-        private void Defend_Click(object sender, RoutedEventArgs e)
-        {
-            if (turnCount == 1)
-            {
-                isDefending = true;
-                defensValue = 0.25;
-                turnCount += oneTurn;
-            }
-            else
-            {
-                TextBox.Text = defenseFailLog;
-            }
-        }
 
-        private void EnemyHealth1_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-            if (isDefending == true && turnCount == 2)
-            {
-                HealthBar.Value -= enemyDmg * defensValue;
-                isDefending = false;
-                turnCount = resetTurn;
-            }
-
-            if (turnCount == 2 && isDefending == false)
-            {
-                HealthBar.Value -= enemyDmg;
-                turnCount = resetTurn;
-            }
-
-        }
-
-        private void HealthBar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-            health = HealthBar.Value;
-        }
-
+        //Hide dungeon window
         private void Close_Dungeon()
         {
-            EnemyHealth1.Visibility = Visibility.Hidden;
+            SkeletonHealth.Visibility = Visibility.Hidden;
             TextBox.Visibility = Visibility.Hidden;
-            ExitButton.Visibility = Visibility.Hidden;
             Attack.Visibility = Visibility.Hidden;
-            Defend.Visibility = Visibility.Hidden;
+            Item.Visibility = Visibility.Hidden;
             Spells.Visibility = Visibility.Hidden;
             Dungeon_Picture.Visibility = Visibility.Hidden;
             UI_Frame.Visibility = Visibility.Hidden;
             UI_Text.Visibility = Visibility.Hidden;
             Skeleton_Text.Visibility = Visibility.Hidden;
+            Return.Visibility = Visibility.Hidden;
             Move_Out.Visibility = Visibility.Visible;
-
-
+            Eat.Visibility = Visibility.Visible;
+            Spell_Menu1.Visibility = Visibility.Hidden;
+            Spell_Menu2.Visibility = Visibility.Hidden;
+            Fireball.Visibility = Visibility.Hidden;
+            Icespear.Visibility = Visibility.Hidden;
+            Earthquake.Visibility = Visibility.Hidden;
+            ThunderStorm.Visibility = Visibility.Hidden;
+            Inventory.Visibility = Visibility.Hidden;
+            GhoulHealth.Visibility = Visibility.Hidden;
+            GhoulText.Visibility = Visibility.Hidden;
+            GiantSpiderHealth.Visibility = Visibility.Hidden;
+            GiantSpiderText.Visibility = Visibility.Hidden;
         }
 
+        //Show dungeon window
         private void GoIntoDungeon()
         {
-            EnemyHealth1.Visibility = Visibility.Visible;
+            SkeletonHealth.Visibility = Visibility.Visible;
             TextBox.Visibility = Visibility.Visible;
-            ExitButton.Visibility = Visibility.Visible;
             Attack.Visibility = Visibility.Visible;
-            Defend.Visibility = Visibility.Visible;
+            Item.Visibility = Visibility.Visible;
             Spells.Visibility = Visibility.Visible;
             Dungeon_Picture.Visibility = Visibility.Visible;
             UI_Frame.Visibility = Visibility.Visible;
             UI_Text.Visibility = Visibility.Visible;
             Skeleton_Text.Visibility = Visibility.Visible;
             Move_Out.Visibility = Visibility.Hidden;
-
+            Return.Visibility = Visibility.Visible;
         }
+
+        //Show dungeon window
+        private void ShowSpells()
+        {
+            Spell_Menu1.Visibility = Visibility.Visible;
+            Spell_Menu2.Visibility = Visibility.Visible;
+            Fireball.Visibility = Visibility.Visible;
+
+            if (iceSpear == true)
+            {
+                Icespear.Visibility = Visibility.Visible;
+            }
+            if (earthQuake == true)
+            {
+                Earthquake.Visibility = Visibility.Visible;
+            }
+            if (thunderStorm == true)
+            {
+                ThunderStorm.Visibility = Visibility.Visible;
+            }
+        }
+
+        //Show dungeon window
+        private void HideSpells()
+        {
+            Spell_Menu1.Visibility = Visibility.Hidden;
+            Spell_Menu2.Visibility = Visibility.Hidden;
+            Fireball.Visibility = Visibility.Hidden;
+
+            if (iceSpear == true)
+            {
+                Icespear.Visibility = Visibility.Hidden;
+            }
+            if (earthQuake == true)
+            {
+                Earthquake.Visibility = Visibility.Hidden;
+            }
+            if (thunderStorm == true)
+            {
+                ThunderStorm.Visibility = Visibility.Hidden;
+            }
+        }
+
+        
         //------------------------------------------------------------------------------Hub--------------------------------------------------------------------\\
         private void Eat_Click(object sender, RoutedEventArgs e)
         {
-            HealthBar.Value += 20;
+            HealthBar.Value += 15;
+            Eat.Visibility = Visibility.Hidden;
         }
 
         private void Shop_Click(object sender, RoutedEventArgs e)
@@ -155,6 +396,91 @@ namespace DungeonCrawler
 
         }
 
+        
+
+        private void Move_Out_Click(object sender, RoutedEventArgs e)
+        {
+            GoIntoDungeon();
+        }
+
+        private void Resume_Menu_Click(object sender, RoutedEventArgs e)
+        {
+            HideMenu();
+
+        }
+
+        private void Exit_Menu_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void Potion_Click(object sender, RoutedEventArgs e)
+        {
+            if (potion > 0)
+            {
+                HealthBar.Value += 20;
+                potion--;
+                PotionCounter.Content = "X " + potion.ToString();
+            }
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ShowMenu();
+            HideOption();
+
+        }
+
+        private void Settings_Click(object sender, RoutedEventArgs e)
+        {
+            ShowOption();
+        }
+
+        //Show option window
+        private void ShowOption()
+        {
+            Music_Option.Visibility = Visibility.Visible;
+            Option.Visibility = Visibility.Visible;
+            Text_Option.Visibility = Visibility.Visible;
+            Sound_Option.Visibility = Visibility.Visible;
+            Exit_Option.Visibility = Visibility.Visible;
+            S_Slider.Visibility = Visibility.Visible;
+            M_Slider.Visibility = Visibility.Visible;
+        }
+
+        //Hide option window
+        private void HideOption()
+        {
+            //Hide Option
+            Music_Option.Visibility = Visibility.Hidden;
+            Option.Visibility = Visibility.Hidden;
+            Text_Option.Visibility = Visibility.Hidden;
+            Sound_Option.Visibility = Visibility.Hidden;
+            Exit_Option.Visibility = Visibility.Hidden;
+            S_Slider.Visibility = Visibility.Hidden;
+            M_Slider.Visibility = Visibility.Hidden;
+        }
+        
+        //Show menu Window
+        private void ShowMenu()
+        {
+            //Show Menu
+            Menu.Visibility = Visibility.Visible;
+            Settings_Menu.Visibility = Visibility.Visible;
+            Exit_Menu.Visibility = Visibility.Visible;
+            Resume_Menu.Visibility = Visibility.Visible;
+        }
+
+        //Hide menu window
+        private void HideMenu()
+        {
+            Menu.Visibility = Visibility.Hidden;
+            Settings_Menu.Visibility = Visibility.Hidden;
+            Exit_Menu.Visibility = Visibility.Hidden;
+            Resume_Menu.Visibility = Visibility.Hidden;
+        }
+        
         //Show shop window
         private void ShowShop()
         {
@@ -174,11 +500,6 @@ namespace DungeonCrawler
             BuyPotion.Visibility = Visibility.Hidden;
             BuyPotionText.Visibility = Visibility.Hidden;
             Move_Out.Visibility = Visibility.Visible;
-        }
-
-        private void Move_Out_Click(object sender, RoutedEventArgs e)
-        {
-            GoIntoDungeon();
         }
     }
 }
